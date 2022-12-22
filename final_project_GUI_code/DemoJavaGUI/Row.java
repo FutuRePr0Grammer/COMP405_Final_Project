868d5c3856fce2336a7142c3ea49d6d6ae53ca84
David Rochon III <59246317+FutuRePr0Grammer@users.noreply.github.com>
2022-12-21 19:37:20 -0500

    Got GUI working for searching by book name. Now just need to change background color to pink and maybe allow to search by genre

--- final_project_GUI_code/DemoJavaGUI/Row.java
+++ final_project_GUI_code/DemoJavaGUI/Row.java
@@ -2,9 +2,17 @@
 // Class to transfer data from database to GUI table
 class Row {
 	public String b_name;
-	public String g_id;
-	Row(String b_name, String g_id) {
+	public String g_name;
+	public String author_name;
+	public String isbn;
+	public String release_date;
+	public int ch_ct;
+	Row(String b_name, String g_name, String author_name, String isbn, String release_date, int ch_ct) {
 		this.b_name = b_name;
-		this.g_id = g_id;
+		this.g_name = g_name;
+		this.author_name = author_name;
+		this.isbn = isbn;
+		this.release_date = release_date;
+		this.ch_ct = ch_ct;
 	}
 }
